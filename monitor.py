3c14e560a05ded143e6b764fc326f7725c96d98d
James Taylor <jamestaylr@gmail.com>
2017-01-04 00:15:51 -0500

    Add Twitter status image upload from URL support

--- monitor.py
+++ monitor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import json, os
+import json, os, base64
 import requests
 import ConfigParser
 import oauth2 as oauth, urllib
@@ -10,7 +10,7 @@ config.read('config.ini')
 
 """ Posts a tweet
 """
-def tweet(status_string):
+def tweet(status_string, media_id):
     if not config.getboolean('twitter', 'should_tweet'):
         print 'Abort posting tweet'
         return
@@ -26,14 +26,18 @@ def tweet(status_string):
         )
     client = oauth.Client(consumer, token)
 
+    data = {
+        'status': status_string,
+        'trim_user': 'true'
+    }
+    if media_id is not None:
+        data['media_ids'] = media_id
+
     # Send the request
     resp, content = client.request(
             'https://api.twitter.com/1.1/statuses/update.json',
             method='POST',
-            body=urllib.urlencode({
-                'status': status_string,
-                'trim_user': 'true'
-            }),
+            body=urllib.urlencode(data),
             headers=None
         )
 
@@ -59,9 +63,44 @@ def get_products(url, limit):
     products = sorted(json['products'], key=lambda x: x['published_at'], reverse=True)
     return products if len(products) != 1 else products[0]
 
+def upload_media(link):
+    r = requests.get(link)
+
+    # Build the OAuth client
+    consumer = oauth.Consumer(
+            key=config.get('twitter', 'consumer_key'),
+            secret=config.get('twitter', 'consumer_secret')
+        )
+    token = oauth.Token(
+            key=config.get('twitter', 'access_token'),
+            secret=config.get('twitter', 'access_secret')
+        )
+    client = oauth.Client(consumer, token)
+
+    # Send the request
+    resp, content = client.request(
+            'https://upload.twitter.com/1.1/media/upload.json',
+            method='POST',
+            body=urllib.urlencode({
+                'media_data': base64.b64encode(r.content),
+            }),
+            headers=None
+        )
+    try:
+        content = json.loads(content)
+        if 'media_id' not in content:
+            raise ValueError
+
+        return content['media_id']
+
+    except ValueError:
+        raise ValueError('No media ID returned')
+
 # Check the content from the subscribed sites
 sites = json.loads(open('data.json').read())['sites']
 for site in sites:
+    print 'Processing site', site['name']
+
     # Get the most recently published product
     try:
         current = get_products(site['url'], 1)
@@ -95,12 +134,21 @@ for site in sites:
             if product['id'] == int(previous[0]):
                 break
 
-        tweet('{} by {} {}{}'.format(
-            product['title'],
-            product['vendor'],
-            site['base_handle'],
-            product['handle']
-        ))
+            media_id = None
+            link = '{}{}'.format(site['base_handle'], product['handle'])
+            link = link.encode('ascii', 'ignore')
+
+            try:
+                if len(product['images']) > 0:
+                    media_id = upload_media(product['images'][0]['src'])
+            except ValueError:
+                print e
+
+            tweet('{} {}'.format(
+                product['title'],
+                link
+            ), media_id)
+
     except requests.exceptions.HTTPError:
         print 'Processing site {} failed'.format(site['name'])
         continue
